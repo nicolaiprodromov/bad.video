367f7c6df743517fdd5273dcef336a726561e1c0
nicolaiprodromov <prodromounicolaie@gmail.com>
2024-07-21 18:57:01 +0300

    test

--- sketch.js
+++ sketch.js
@@ -510,7 +510,7 @@ var SCROLLDELAY = 80;
 
 if (window?.navigator?.platform  === 'MacIntel'){
     console.log("IS FUCJKING IOS")
-    SCROLLDELAY = 15;
+    SCROLLDELAY = 18;
 }
 
 function debounce322(func, delay){
@@ -522,7 +522,7 @@ function debounce322(func, delay){
 }
 
 
-console.log("SCROLLDELAY", SCROLLDELAY)
+console.log("SCROLLDELAY", SCROLLDELAY);
 window.addEventListener('wheel', debounce(scrollSection, SCROLLDELAY));
 window.addEventListener('wheel', debounce(rawScrollSection, SCROLLDELAY));
 
