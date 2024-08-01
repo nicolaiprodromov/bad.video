b2d609ea7f3c94d4f372ee36b3e66f682ec68fda
nicolaiprodromov <prodromounicolaie@gmail.com>
2024-08-01 21:11:52 +0300

    click handler cv button

--- about/about.js
+++ about/about.js
@@ -1,6 +1,19 @@
 import { Page } from '/lib/page_builder.js'
 
 const PAGE_ABOUT_BUILDER = new Page(22);
+PAGE_ABOUT_BUILDER.pd = {
+    about_button : document.querySelector(".about_cv_button"),
+}
 PAGE_ABOUT_BUILDER.scroll_handler = async (pd) => {
     
+}
+PAGE_ABOUT_BUILDER.first_scroll_handler = async (pd) => {
+    pd.addEventListener('click', () => {
+        const a = document.createElement('a')
+        a.href = url
+        a.download = url.split('/').pop()
+        document.body.appendChild(a)
+        a.click()
+        document.body.removeChild(a)
+    }, false)
 }
