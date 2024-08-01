f5ae1bdcf4e636395fcf098f24783062d3090034
nicolaiprodromov <prodromounicolaie@gmail.com>
2024-08-01 19:09:42 +0300

    continue styling

--- page9/page9.js
+++ page9/page9.js
@@ -55,48 +55,23 @@ PAGE9_BUILDER.resize_handler = (pd) => {
 PAGE9_BUILDER.first_scroll_handler = async (pd) => {
     setTimeout(() => {
         var tw0 = document.querySelector("#twitter-widget-0");
-        var tw1 = document.querySelector("#twitter-widget-1");
-        var tw2 = document.querySelector("#twitter-widget-2");
-        var ccc = document.querySelectorAll(".twitter-tweet")
-        for (var c_c of ccc){
-            //c_c.style.maxWidth = "350px"
-        }
-
         var rrh = tw0.getBoundingClientRect().width/tw0.getBoundingClientRect().height
-
-        tw0.style.width = `${pd.holder.getBoundingClientRect().height*rrh}px`
-        tw1.style.width = `${pd.holder.getBoundingClientRect().height*rrh}px`
-        tw2.style.width = `${pd.holder.getBoundingClientRect().height*rrh}px`
-
-        tw0.style.maxHeight = "100%"
-        tw1.style.maxHeight = "100%"
-        tw2.style.maxHeight = "100%"
-        // tw0.style.pointerEvents = "none"
-        // tw1.style.pointerEvents = "none"
-        // tw2.style.pointerEvents = "none"
-        tw0.style.margin = "0px"
-        tw1.style.margin = "0px"
-        tw2.style.margin = "0px"
+        tw0.style.maxHeight      = "100%"
+        tw0.style.width          = `${pd.holder.getBoundingClientRect().height*rrh}px`
+        tw0.style.pointerEvents  = "none"
+        tw0.style.margin         = "0px"
         tw0.style.alignItems     = "center"
         tw0.style.justifyContent = "center"
-        tw0.style.position       = "absolute"
+        tw0.style.position       = "relative"
+
+        var tw1 = document.querySelector("#twitter-widget-1");
+        var rrh = tw1.getBoundingClientRect().width/tw1.getBoundingClientRect().height
+        tw1.style.maxHeight      = "100%"
+        tw1.style.width          = `${pd.holder.getBoundingClientRect().height*rrh}px`
+        tw1.style.pointerEvents  = "none"
+        tw1.style.margin         = "0px"
         tw1.style.alignItems     = "center"
         tw1.style.justifyContent = "center"
-        tw1.style.position       = "absolute"
-        tw2.style.alignItems     = "center"
-        tw2.style.justifyContent = "center"
-        tw2.style.position       = "absolute"
-
-        tw0.style.left      = `calc(50% - ${tw0.getBoundingClientRect().width/2}px - 100px)`
-        tw0.style.top       = "50%"
-        // tw0.style.transform = "translate(-50%,-50%) rotate(-2deg)"
-
-        tw1.style.left      = `calc(50% + ${tw1.getBoundingClientRect().width/2}px + 100px)`
-        tw1.style.top       = "50%"
-        // tw1.style.transform = "translate(-50%,-50%) rotate(-1deg)"
-
-        tw2.style.left      = `calc(50%)`
-        tw2.style.top       = "50%"
-        // tw2.style.transform = "translate(-50%,-50%) rotate(3deg)"
-    }, 3000)
+        tw1.style.position       = "relative"
+    }, 300)
 }
