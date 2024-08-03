06c70b2d2e4e98f27a75a0e099954a4c3cdf3648
nicolaiprodromov <prodromounicolaie@gmail.com>
2024-08-03 13:54:36 +0300

    cv change

--- about/about.js
+++ about/about.js
@@ -9,13 +9,26 @@ PAGE_ABOUT_BUILDER.scroll_handler = async (pd) => {
 }
 PAGE_ABOUT_BUILDER.first_scroll_handler = async (pd) => {
     pd.about_button.addEventListener('click', () => {
-        const url = "https://raw.githubusercontent.com/nicolaiprodromov/bad.video/master/Nicolaie%20Prodromou[CV].pdf"
-        const a = document.createElement('a')
-        a.href = url
-        a.target = "blank"
-        a.download = url.split('/').pop()
-        document.body.appendChild(a)
-        a.click()
-        document.body.removeChild(a)
+        setTimeout(() => {
+            const url = "https://raw.githubusercontent.com/nicolaiprodromov/bad.video/master/Nicolaie%20Prodromou[CV][d].pdf"
+            const a = document.createElement('a')
+            a.href = url
+            a.target = "blank"
+            a.download = url.split('/').pop()
+            document.body.appendChild(a)
+            a.click()
+            document.body.removeChild(a)
+        }, 100);
+        setTimeout(() => {
+            const url = "https://raw.githubusercontent.com/nicolaiprodromov/bad.video/master/Nicolaie%20Prodromou[CV][l].pdf"
+            const a = document.createElement('a')
+            a.href = url
+            a.target = "blank"
+            a.download = url.split('/').pop()
+            document.body.appendChild(a)
+            a.click()
+            document.body.removeChild(a)
+        }, 200);
+
     }, false)
 }
